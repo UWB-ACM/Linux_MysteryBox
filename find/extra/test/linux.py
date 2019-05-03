4383ee17ffe297f256e50706f88ecd412d6da181
Lizzy Presland <lizzy.presland@gmail.com>
2019-05-03 02:15:00 -0700

    multi-track confusion

---
+++ find/extra/test/linux.py
@@ -0,0 +1,7 @@
+def testiboi():
+    print("-------MIC CHECK 1-2-3-------")
+    print("-----insert static here------")
+    print("uh hello, can anyone hear me?")
+
+if __name__ == "__main__":
+    testiboi()
